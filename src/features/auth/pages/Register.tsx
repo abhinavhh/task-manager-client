dfc2e36873ed45ebe8774793aa905b4add8e8d05
Abhinav <abhinavponnu964531@gmail.com>
2025-09-24 15:36:45 +0530

    Updated structure added features

---
+++ src/features/auth/pages/Register.tsx
@@ -0,0 +1,14 @@
+
+const Register = () => {
+  return (
+    <div className="">
+
+        <form action="">
+            <label htmlFor="">username</label>
+        </form>
+
+    </div>
+  )
+}
+
+export default Register
