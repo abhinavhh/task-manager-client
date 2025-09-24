987d867e947e696cc20f7e69a1e473b813d33d85
Abhinav <abhinavponnu964531@gmail.com>
2025-09-24 10:30:05 +0530

    Created a useAUthStore to handle role access

--- src/store/useAuthStore.tsx
+++ src/store/useAuthStore.tsx
@@ -1,6 +1,39 @@
-type Role = "ADMIN" | "USER" | null
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type Role = "ADMIN" | "USER" | null;
 
 interface AuthState {
-    accessToken: string;
-    refreshToekn: string;
+  accessToken: string | null;
+  refreshToken: string | null;
+  userRole: Role;
+  setAuth: (accessToken: string, refreshToekn: string, userRole: Role) => void;
+  clearAuth: () => void;
 }
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      refreshToken: null,
+      userRole: null,
+
+      // save token + role
+      setAuth: (accessToken, refreshToken, userRole) =>
+        set(() => ({
+          accessToken,
+          refreshToken,
+          userRole,
+        })),
+      clearAuth: () =>
+        set(() => ({
+          accessToken: null,
+          refreshToekn: null,
+          userRole: null,
+        })),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
