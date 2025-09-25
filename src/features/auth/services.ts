97dfeb3fce02d44b1abedb610e33cba4f60d1265
Abhinav <abhinavponnu964531@gmail.com>
2025-09-25 09:32:03 +0530

    Added login register and refresh token api services

---
+++ src/features/auth/services.ts
@@ -0,0 +1,52 @@
+import { axiosInstance } from "@/lib/utils";
+import { useAuthStore } from "@/store/useAuthStore";
+
+interface LoginPayload{
+    username: string;
+    password: string;
+}
+
+interface RegisterPayload{
+    username: string;
+    password: string;
+    email: string;
+}
+
+interface ResponsePayload{
+    accessToken: string;
+    refreshToken: string;
+    role: "USER" | "ADMIN";
+}
+
+const authService = {
+
+
+    login: async(payload: LoginPayload): Promise<ResponsePayload> => {
+        const { data } = await axiosInstance.post('/auth/login', payload);
+        useAuthStore.getState().setAuth(data.access, data.refresh, data.role);
+        return data;
+    },
+
+    register: async(payload: RegisterPayload): Promise<string> => {
+        const { data } = await axiosInstance.post('/auth/register', payload);
+        return data;
+    },
+
+    refreshToken: async(): Promise<string> => {
+        const refreshToken = useAuthStore.getState().refreshToken;
+        if(!refreshToken) throw new Error("No refresh token available");
+
+        const { data } = await axiosInstance.post<{accessToken: string}>("/auth/refresh", {
+            refreshToken
+        })
+
+        useAuthStore.getState().setAuth(data.accessToken, refreshToken, useAuthStore.getState().userRole)
+        return data.accessToken;
+    },
+
+    logOut: () => {
+        useAuthStore.getState().clearAuth();
+    }
+}
+
+export default authService;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const baseUri = import.meta.env.VITE_BACKEND_URL
+
+export const axiosInstance = axios.create({
+  baseURL: baseUri,
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+
